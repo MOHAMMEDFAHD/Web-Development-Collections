230cb0fac49db186be41e762c6bbfea0a73e1f89
Mohammed Fahd Abrah <93455327+MOHAMMEDFAHD@users.noreply.github.com>
2026-05-21 23:02:46 +0800

    Update connection.php

--- google-calendar-clone/connection.php
+++ google-calendar-clone/connection.php
@@ -1,6 +1,1 @@
-<?php
 
-// 1. Connect to Local MySQL Server (using XAMPP or MAMPP)
-$username = "root";
-$conn = new mysqli("localhost", $username, "", "course_calendar");
-$conn->set_charset("utf8mb4");
